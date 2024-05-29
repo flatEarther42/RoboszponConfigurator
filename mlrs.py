a80c59bf4de13db65991f8f6d0aa2d4f2d505087
Tomek Zebrowski <zebromek@gmail.com>
2024-05-29 18:05:57 +0200

    Add realtime roboszpon info

--- mlrs.py
+++ mlrs.py
@@ -1,20 +1,34 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox
-from PyQt5.QtCore import QStringListModel, QModelIndex
+from PyQt5.QtCore import QStringListModel, QModelIndex, QTimer
 from PyQt5 import uic
 
 import roboszpon_lib
+import can, time
+
+CONNECTION_TIMEOUT = 1.0
+
+
+class Roboszpon:
+    node_id: int
+    mode: str
+    flags: int
+    timestamp: float
 
 
 class MyLittleRoboszponSuite(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi("app.ui", self)
-        self.roboszpon = 0
+
+        self.canbus = can.interface.Bus("can0", interface="socketcan")
+
+        self.roboszpon = None
         self.armed = False
 
         self.deviceList = QStringListModel()
-        self.devices = []
+        self.deviceIds = []
+        self.devices = {}
         self.deviceListView.setModel(self.deviceList)
         self.deviceListView.clicked.connect(self.deviceListClicked)
 
@@ -24,63 +38,105 @@ class MyLittleRoboszponSuite(QMainWindow):
         self.positionButton.clicked.connect(self.positionButtonClicked)
         self.stopAllButton.clicked.connect(self.stopAllButtonClicked)
 
-        self.addConnectedDevice(1)
-        self.addConnectedDevice(2)
-        roboszpon_lib.send_can_frame("can0", 0x01, 0x003F000000000000)
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.tick)
+        self.timer.start(100)
 
-        # Connect the submit button to the submit_parameters method
+        can.Notifier(self.canbus, [self])
 
-    #     self.submit_button.clicked.connect(self.submit_parameters)
+    def __del__(self):
+        self.canbus.shutdown()
+
+    def __call__(self, message):
+        self.on_message_received(message)
+
+    def tick(self):
+        if self.roboszpon is not None:
+            if (
+                self.devices[self.roboszpon].timestamp + CONNECTION_TIMEOUT
+                < time.time()
+            ):
+                self.connectionLabel.setText(" Connection: LOST")
+            self.stateLabel.setText(
+                f" Operation state: {roboszpon_lib.ROBOSZPON_MODES[self.devices[self.roboszpon].mode]}"
+            )
+            self.flagsLabel.setText(
+                f" Flags: {(bin(self.devices[self.roboszpon].flags + (1 << 33)))[3:]}"
+            )
+
+    def on_message_received(self, message: can.message.Message):
+        if message.is_extended_id or message.dlc != 8:
+            print("Received invalid CAN message")
+        msg = roboszpon_lib.decode_message(
+            message.arbitration_id, int.from_bytes(message.data, "big")
+        )
+        if msg["message_id"] == roboszpon_lib.MSG_STATUS_REPORT:
+            node_id = msg["node_id"]
+            if node_id not in self.deviceIds:
+                self.addConnectedDevice(node_id)
+            roboszpon = self.devices[msg["node_id"]]
+            roboszpon.mode = msg["mode"]
+            roboszpon.flags = msg["flags"]
+            roboszpon.timestamp = message.timestamp
+            if msg["node_id"] == self.roboszpon:
+                self.connectionLabel.setText(" Connection: OK")
+
     def addConnectedDevice(self, node_id):
         self.deviceList.insertRow(self.deviceList.rowCount())
         index = self.deviceList.index(self.deviceList.rowCount() - 1)
         self.deviceList.setData(index, f"Roboszpon 0x{node_id:02x}", 0)
-        self.devices.append(node_id)
-        print(self.devices)
+        self.deviceIds.append(node_id)
+        self.devices[node_id] = Roboszpon()
+        self.devices[node_id].node_id = node_id
+        print(self.deviceIds)
 
     def removeConnectedDevice(self, node_id):
-        if node_id not in self.devices:
+        if node_id not in self.deviceIds:
             raise KeyError("can't remove node. there is no such node")
-        index = self.devices.index(node_id)
+        index = self.deviceIds.index(node_id)
         self.deviceList.removeRow(index)
-        self.devices.remove(node_id)
-        print(self.devices)
+        self.deviceIds.remove(node_id)
+        print(self.deviceIds)
 
     def selectDevice(self, node_id):
         self.roboszpon = node_id
-        # set self.armed to armed or disarmed accordingly
+        self.armed = self.devices[node_id].mode != roboszpon_lib.ROBOSZPON_MODE_STOPPED
+        if self.armed:
+            self.armButton.setText("Disarm")
+        else:
+            self.armButton.setText("Arm")
         print(f"Selected roboszpon: {node_id}")
 
     def deviceListClicked(self, index: QModelIndex):
-        self.selectDevice(self.devices[index.row()])
+        self.selectDevice(self.deviceIds[index.row()])
 
     def armButtonClicked(self):
         self.armed = not self.armed
         if self.armed:
-            roboszpon_lib.disarm("can0", self.roboszpon)
+            roboszpon_lib.disarm(self.canbus, self.roboszpon)
             self.armButton.setText("Arm")
         else:
-            roboszpon_lib.arm("can0", self.roboszpon)
+            roboszpon_lib.arm(self.canbus, self.roboszpon)
             self.armButton.setText("Disarm")
 
     def stopAllButtonClicked(self):
-        roboszpon_lib.emergency_stop("can0")
+        roboszpon_lib.emergency_stop(self.canbus)
         self.armed = False
         self.armButton.setText("Disarm")
 
     def dutyButtonClicked(self):
         roboszpon_lib.send_duty_command(
-            "can0", self.roboszpon, self.dutySpinBox.value()
+            self.canbus, self.roboszpon, self.dutySpinBox.value()
         )
 
     def velocityButtonClicked(self):
         roboszpon_lib.send_velocity_command(
-            "can0", self.roboszpon, self.velocitySpinBox.value()
+            self.canbus, self.roboszpon, self.velocitySpinBox.value()
         )
 
     def positionButtonClicked(self):
         roboszpon_lib.send_position_command(
-            "can0", self.roboszpon, self.positionSpinBox.value()
+            self.canbus, self.roboszpon, self.positionSpinBox.value()
         )
 
 
--- roboszpon_lib.py
+++ roboszpon_lib.py
@@ -3,21 +3,27 @@ import struct
 
 MSG_MOTOR_COMMAND = 0x01
 MSG_ACTION_REQUEST = 0x02
+MSG_STATUS_REPORT = 0x03
+MSG_AXIS_REPORT = 0x04
+MSG_MOTOR_REPORT = 0x05
 
 ACTION_ARM = 0x00
 ACTION_DISARM = 0x01
 
+ROBOSZPON_MODE_STOPPED = 0x00
+ROBOSZPON_MODE_RUNNING = 0x01
+ROBOSZPON_MODE_ERROR = 0x02
+ROBOSZPON_MODES = {0: "STOPPED", 1: "RUNNING", 2: "ERROR"}
 
-def send_can_frame(interface, frame_id, data):
+
+def send_can_frame(canbus, frame_id, data):
     try:
-        canbus = can.interface.Bus(channel=interface, bustype="socketcan")
         frame = can.Message(
             arbitration_id=frame_id,
             data=data.to_bytes(8, byteorder="big"),
             is_extended_id=False,
         )
         canbus.send(frame)
-        canbus.shutdown()
     except Exception as e:
         print(f"Error sending CAN frame: {e}")
 
@@ -26,16 +32,49 @@ def build_frame_id(node_id, message_id):
     return ((node_id & 0b11111) << 6) + (message_id & 0b111111)
 
 
-def send_action_request(interface, node_id, action_id):
-    send_can_frame(interface, build_frame_id(node_id, MSG_ACTION_REQUEST), action_id)
+def decode_message(frame_id, data):
+    node_id = (frame_id >> 6) & 0b11111
+    message_id = frame_id & 0b111111
+    if message_id == MSG_STATUS_REPORT:
+        mode = (data >> 62) & 0b11
+        flags = data & 0x3FFFFFFFFFFFFFFF
+        return {
+            "node_id": node_id,
+            "message_id": message_id,
+            "mode": mode,
+            "flags": flags,
+        }
+    if message_id == MSG_AXIS_REPORT:
+        position = (data >> 32) & 0xFFFFFFFF
+        velocity = data & 0xFFFFFFFF
+        return {
+            "node_id": node_id,
+            "message_id": message_id,
+            "position": position,
+            "velocity": velocity,
+        }
+    if message_id == MSG_MOTOR_REPORT:
+        current = (data >> 32) & 0xFFFFFFFF
+        duty = data & 0xFFFFFFFF
+        return {
+            "node_id": node_id,
+            "message_id": message_id,
+            "current": current,
+            "duty": duty,
+        }
+    return {"node_id": node_id, "message_id": message_id, "data": data}
 
 
-def arm(interface, node_id):
-    send_action_request(interface, node_id, ACTION_ARM)
+def send_action_request(canbus, node_id, action_id):
+    send_can_frame(canbus, build_frame_id(node_id, MSG_ACTION_REQUEST), action_id)
 
 
-def disarm(interface, node_id):
-    send_action_request(interface, node_id, ACTION_DISARM)
+def arm(canbus, node_id):
+    send_action_request(canbus, node_id, ACTION_ARM)
+
+
+def disarm(canbus, node_id):
+    send_action_request(canbus, node_id, ACTION_DISARM)
 
 
 def float_to_bits(value):
@@ -43,24 +82,24 @@ def float_to_bits(value):
     return struct.unpack("i", value_bits)[0]
 
 
-def send_motor_command(interface, node_id, motor_command_type, command):
+def send_motor_command(canbus, node_id, motor_command_type, command):
     data = ((motor_command_type & 0xFF) << 56) + (
         (float_to_bits(command) & 0xFFFFFFFF) << 24
     )
-    send_can_frame(interface, build_frame_id(node_id, MSG_MOTOR_COMMAND), data)
+    send_can_frame(canbus, build_frame_id(node_id, MSG_MOTOR_COMMAND), data)
 
 
-def send_duty_command(interface, node_id, command):
-    send_motor_command(interface, node_id, 0, command)
+def send_duty_command(canbus, node_id, command):
+    send_motor_command(canbus, node_id, 0, command)
 
 
-def send_velocity_command(interface, node_id, command):
-    send_motor_command(interface, node_id, 1, command)
+def send_velocity_command(canbus, node_id, command):
+    send_motor_command(canbus, node_id, 1, command)
 
 
-def send_position_command(interface, node_id, command):
-    send_motor_command(interface, node_id, 2, command)
+def send_position_command(canbus, node_id, command):
+    send_motor_command(canbus, node_id, 2, command)
 
 
-def emergency_stop(interface):
-    send_can_frame(interface, 0x001, 0)
+def emergency_stop(canbus):
+    send_can_frame(canbus, 0x001, 0)
